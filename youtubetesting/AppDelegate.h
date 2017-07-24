bb4bbb2c6b088d63ee9586be68e272cce058bfbb
Nicky Patson <nicky.patson@cloudium.io>
2017-07-24 11:28:32 +0530

    Initial Commit

---
+++ youtubetesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  youtubetesting
+//
+//  Created by Nicky on 7/24/17.
+//  Copyright © 2017 Nicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ youtubetesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  youtubetesting
+//
+//  Created by Nicky on 7/24/17.
+//  Copyright © 2017 Nicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
